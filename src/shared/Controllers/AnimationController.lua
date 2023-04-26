702e2f18f618381e54559860e9b915150fd32b06
microwavedram <89029844+microwavedram@users.noreply.github.com>
2023-04-26 23:20:16 +0000

    animation updates

--- src/shared/Controllers/AnimationController.lua
+++ src/shared/Controllers/AnimationController.lua
@@ -22,24 +22,35 @@ local function merge(x,y)
     return t
 end
 
-local function GetPoseData(Keyframe, Parent)
-
+local function GetPoseData(Keyframe, Parent: Keyframe | Pose, Path)
+    local Time = 0
     if Parent:IsA("Keyframe") then
-        Keyframe[]
+        Time = Parent.Time
+        Path = "/"
+    else
+        if Parent.Weight == 1 then
+            Keyframe[Path] = Parent.CFrame
+        end
     end
 
     for _,Pose in pairs(Parent:GetChildren()) do
-        Keyframe = merge(Keyframe,GetPoseData(Keyframe, Pose))
+        Keyframe = merge(Keyframe,(GetPoseData(Keyframe, Pose, `{Path}{Pose.Name}/`)))
     end
     
-    return Keyframe
+    return Keyframe, Time
 end
 
 function AnimationController:LoadAnimation(Model, KeyframeSequence: KeyframeSequence)
     local Keyframes = {}
     
+    for _,RobloxKeyframe in pairs(KeyframeSequence:GetChildren()) do
+        local Keyframe, Time =  GetPoseData({}, RobloxKeyframe)
+        Keyframes[#Keyframes+1] = {Time = Time, Keyframe = Keyframe}
+    end
     
-
+    Keyframes = table.sort(Keyframes, function(a,b)
+        return a.Time < b.Time
+    end)
 
     local AnimationTrack = Animation.new(Model, KeyframeSequence.Name, KeyframeSequence.Loop, Keyframes)
 
--- src/shared/Modules/Animation.lua
+++ src/shared/Modules/Animation.lua
@@ -1,8 +1,20 @@
-local RunService = game:GetService("RunService")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local HttpService = game:GetService("HttpService")
+local RunService = game:GetService("RunService")
+
+local TableUtil = require(ReplicatedStorage.fKit.Packages.TableUtil)
 
 local AnimationTrack = {}
 AnimationTrack.__index = AnimationTrack
+
+local function isInTable(t,v)
+    for _,value in pairs(t) do
+        if value == v then
+            return true
+        end
+    end
+    return false
+end
 
 function AnimationTrack.new(Model, Name, Keyframes, Loop)
     local self = setmetatable({}, AnimationTrack)
@@ -14,17 +26,69 @@ function AnimationTrack.new(Model, Name, Keyframes, Loop)
     
     self.Instance = Model
     self.Time = 0
+    self.Speed = 0
 
     return self
 end
 
-function AnimationTrack:GetStateAtTime()
+function AnimationTrack:FindPreviousKeyframe(Time, Path)
+    for _,Keyframe in pairs(TableUtil.Reverse(self.Keyframes)) do
+        if Keyframe.Time < Time then
+            if Keyframe.Keyframe[Path].Weight == 1 then
+                return Keyframe
+            end
+        end
+    end
+end
+
+function AnimationTrack:FindNextKeyframe(Time, Path)
+    for _,Keyframe in pairs(self.Keyframes) do
+        if Keyframe.Time > Time then
+            if Keyframe.Keyframe[Path].Weight == 1 then
+                return Keyframe
+            end
+        end
+    end
+end
+
+function AnimationTrack:GetAllJoints()
+    local Joints = {}
+    for _,Keyframe in pairs(self.Keyframes) do
+        for Path,_ in pairs(Keyframe.Keyframe) do
+            if not isInTable(Joints, Path) then
+                table.insert(Joints, Path)
+            end
+        end
+    end
+    return Joints
+end
+
+function AnimationTrack:GetInstanceFromPath(Root,Path)
+    local Sections: table = select(2,string.split(Path, "/"))
+
+    local Item = Root
+    for _,Section in pairs(Sections) do
+        Item = Item:FindFirstChild(Section)
+    end
     
+    return Item
+end
+
+function AnimationTrack:SetStateAtTime(Time)
+    for _,JointPath in pairs(self:GetAllJoints()) do
+        local Joint = self:GetInstanceFromPath(self.Instance, JointPath)
+
+        local Previous = self:FindPreviousKeyframe(Time, JointPath)
+        local Next = self:FindNextKeyframe(Time, JointPath)
+
+        Joint.CFrame = Previous.Keyframe[JointPath]:Lerp(Next.Keyframe[JointPath], (Time-Previous.Time)/(Next.Time-Previous.Time))
+    end
 end
 
 function AnimationTrack:Play()
     RunService:BindToRenderStep(`fKit.Animation.{HttpService:GenerateGUID(false)}`, Enum.RenderPriority.Camera.Value + 1, function(dt)
-        self.Time = self.Time + dt
+        self.Time = self.Time + (dt) * self.Speed
+        self:SetStateAtTime(self.Time)
     end)
 end
 
