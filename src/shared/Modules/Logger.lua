07eff6e8fd0fab66efaa4a1e1ff765b1dcb685fd
microwavedram <89029844+microwavedram@users.noreply.github.com>
2023-05-03 07:50:35 +0100

    Logger Module

--- src/shared/Modules/Logger.lua
+++ src/shared/Modules/Logger.lua
@@ -6,30 +6,38 @@ function Logger.new(LogName, Debug, LanguageParser)
 
     self.LogName = LogName
     self.IsDebug = Debug or false
-    self._languageParser = LanguageParser or function(m) return m end
+    self._languageParser = LanguageParser or function(m)
+        return m
+    end
 
     return self
 end
 
 function Logger:Fatal(message,...)
-    error(`[FATAL] [{os.date("%X",os.time())}] [{self.LogName}] [{getfenv(2).script.Name}] : {self._languageParser(message,...)}`)
+    error(`[FATAL] [{self.LogName}] [{getfenv(2).script.Name}] : {self._languageParser(message,...)}`)
 end
 
 function Logger:Error(message,...)
-    warn(`[ERROR] [{os.date("%X",os.time())}] [{self.LogName}] [{getfenv(2).script.Name}] : {self._languageParser(message,...)}`)
+    warn(`[ERROR] [{self.LogName}] [{getfenv(2).script.Name}] : {self._languageParser(message,...)}`)
 end
 
 function Logger:Warn(message,...)
-    warn(`[WARN] [{os.date("%X",os.time())}] [{self.LogName}] [{getfenv(2).script.Name}] : {self._languageParser(message,...)}`)
+    warn(`[WARN] [{self.LogName}] [{getfenv(2).script.Name}] : {self._languageParser(message,...)}`)
 end
 
 function Logger:Debug(message,...)
     if self.IsDebug == false then return end
-    warn(`[DEBUG] [{os.date("%X",os.time())}] [{self.LogName}] [{getfenv(2).script.Name}] : {self._languageParser(message,...)}`)
+    warn(`[DEBUG] [{self.LogName}] [{getfenv(2).script.Name}] : {self._languageParser(message,...)}`)
+end
+
+function Logger:Assert(condition, message,...)
+    if not condition then
+        error(`[ASSERT] [{self.LogName}] [{getfenv(2).script.Name}] : {self._languageParser(message,...)}`)
+    end
 end
 
 function Logger.__call(self, message,...)
-    print(`[LOG] [{os.date("%X",os.time())}] [{self.LogName}] [{getfenv(2).script.Name}] : {self._languageParser(message,...)}`)
+    print(`[LOG] [{self.LogName}] [{getfenv(2).script.Name}] : {self._languageParser(message,...)}`)
 end
 
 return Logger
