d30898787783ae05f5b84d0c266155e40397be14
microwavedram <89029844+microwavedram@users.noreply.github.com>
2023-05-03 07:50:45 +0100

    Tool Link

--- src/shared/Components/@fKit.Gun.lua
+++ src/shared/Components/@fKit.Gun.lua
@@ -5,32 +5,20 @@ local Knit = require(ReplicatedStorage.fKit.Packages.Knit)
 local Component = require(ReplicatedStorage.fKit.Packages.Component)
 local LocalTool = require(ReplicatedStorage.fKit.Common.Extensions.LocalTool)
 
-local GunController = Knit.GetController("GunController")
+local WeaponController = Knit.GetController("GunController")
 
 local fKitGun = Component.new{ Tag = "@fKit.Gun", Ancestors = { Players, workspace }, Extensions = { LocalTool } }
 
 local LocalPlayer = Players.LocalPlayer
 
-function fKitGun:Construct()
-    
-end
-
 function fKitGun:Start()
     self.Instance.AncestryChanged:Connect(function()
         if self.Instance.Parent == LocalPlayer.Character then
-            GunController:EquipGun(self.Instance.Gun.Value)
-        elseif GunController.CurrentGun == self.Instance.Gun.Value then
-            GunController:UnEquipGun()
+            self.GunId = WeaponController:EquipWeapon(self.Instance.Gun.Value, self.GunId)
+        elseif WeaponController.CurrentWeapon == self.Instance.Gun.Value then
+            WeaponController:UnEquipWeapon(self.GunId)
         end
     end)
-end
-
-function fKitGun:SteppedUpdate(dt)
-
-end
-
-function fKitGun:Stop()
-    
 end
 
 return fKitGun
